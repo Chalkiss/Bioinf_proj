97a3721c6baff099fa9dcba0c532182d61b6c240
FGap <filgap@wp.pl>
2020-07-23 20:13:16 +0200

    Dodaj pliki projektów.

---
+++ Bioinf_proj/Bioinf_proj.cpp
@@ -0,0 +1,22 @@
+﻿#include <fstream>
+#include <vector>
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+std::vector<string> snippets;
+int main() {
+    ifstream openFile;
+
+    openFile.open("C:\\Users\\filga\\Documents\\cpp projects\\input.txt");
+
+    if (openFile.is_open()) {
+        string tmp;
+        while (getline(openFile, tmp)) {
+            snippets.push_back(tmp);
+        }
+    }
+    for (int i = 0; i < snippets.size(); i++)
+        cout << snippets[i]<<endl;
+}
